128eb7ccc03bddb9cd6c5d27ba27d7a6fa3aab4e
Adonis Imperial <adonisimperial@yahoo.com>
2026-04-23 15:21:40 +0800

    Fonts and image optimization

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,24 @@
 import "./globals.css";
+{/* use from "next/font/google" for downloaded fonts */}
+import { Nunito, Orbitron, Montserrat_Alternates } from "next/font/google"
+
+const nunito = Nunito({
+  subsets: ["latin"],
+  display: "swap"
+})
+
+const orbitron = Orbitron({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-orbitron"
+})
+
+const montserratAlternates = Montserrat_Alternates({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["100", "200", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-montserrat-alternates"
+})
 
 export default function RootLayout({
   children,
@@ -6,8 +26,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="min-h-full flex flex-col">
+    <html lang="en" className={`${nunito.className} ${montserratAlternates.variable} ${orbitron.variable} min-h-full flex flex-col`}>
+      {/* className={`nunito.className`} for single font*/}
+      <body>
         {children}
         </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+{/* use this to use the Next.js Image component */}
+{/* import Image from "next/image" */}
+import heroImage from "@/public/adonis-space.png"
+
 export default function Home() {
   return (
     <main>
       <section className="flex flex-col-reverse md:flex-row items-center justify-center gap-8 px-6 py-12 mx-auto max-w-7xl overflow-hidden">
         <div className="flex-1 space-y-6 text-center md:text-left">
-          <h1>Discover what's possible with 3d printing</h1>
+          <h1>Discover what's possible with <span className="three">3d</span> printing</h1>
+
           <p>Join our community of creators and explore a vast library of user-submitted models.</p>
           
           <button className="rotate-trigger">
@@ -12,9 +17,29 @@ export default function Home() {
         </div>
         
         {/*alternative donut shape className="w-64 h-64 rounded-full object-cover mask-radial-from-transparent mask-radial-from-10% mask-radial-to-black mask-radial-to-31%"*/}
+        {/*
         <img
           className="donut-shockwave"
           src="/adonis-space.png"
+          alt="Adonis view earth from the space"
+        />
+        */}
+
+        {/* Using the Image component*/}
+        {/*
+        <Image
+          className="donut-shockwave"
+          src="/adonis-space.png"
+          alt="Adonis view earth from the space"
+          width={100}
+          height={100}
+        />
+        */}
+
+        {/* Using the import heroImage*/}
+        <img
+          className="donut-shockwave"
+          src={heroImage.src}
           alt="Adonis view earth from the space"
         />
       </section>
